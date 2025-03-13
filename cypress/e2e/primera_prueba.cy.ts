93b2ae1be46d1b61fa1c711edf90bedfa3b896bf
Mireya Alvarez <malvarez-ctr@semantic-ai.com>
2025-03-13 15:33:24 -0700

    Nuevos cambios marzo 13

--- cypress/e2e/primera_prueba.cy.ts
+++ cypress/e2e/primera_prueba.cy.ts
@@ -2,6 +2,8 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('/');
 
-    cy.get('h1').should('be.visible').and('contain', 'Lista de Actividades');
+    // cy.wait(10000);
+    cy.get('h1', {timeout:10000}).should('be.visible').and('contain', 'Lista de Actividades');
+
   })
 })
